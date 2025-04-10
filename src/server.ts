4552cd0ecde4c22466832ad891df445bcc89fd1c
Bora Kilicoglu <sbkilicoglu@gmail.com>
2025-04-11 02:48:14 +0300

    feat: hello-world

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,3 @@
-// Error: server.ts doesn't seem to be Netlify compatible and is not known default. Please replace it with Netlify compatible server.ts.
-
-// It seems like you use "CommonEngine" - for this case your server.ts file should contain following:
-
-import { CommonEngine } from '@angular/ssr/node';
-import { render } from '@netlify/angular-runtime/common-engine';
-
-const commonEngine = new CommonEngine();
-
-export async function netlifyCommonEngineHandler(
-  request: Request,
-  context: any
-): Promise<Response> {
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
-
-  return await render(commonEngine);
-}
-
-// If you want to use "AppEngine" instead - your server.ts file should contain following:
-
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 import { getContext } from '@netlify/angular-runtime/context';
 
@@ -32,14 +7,6 @@ export async function netlifyAppEngineHandler(
   request: Request
 ): Promise<Response> {
   const context = getContext();
-
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
-
   const result = await angularAppEngine.handle(request, context);
   return result || new Response('Not found', { status: 404 });
 }
