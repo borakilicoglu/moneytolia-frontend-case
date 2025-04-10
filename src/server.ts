f3c8bb5eaf53a94aaac39144ea01a6ed1f6b0fed
Bora Kilicoglu <sbkilicoglu@gmail.com>
2025-04-11 02:53:05 +0300

    feat: hello-world

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,14 @@ export async function netlifyAppEngineHandler(
   request: Request
 ): Promise<Response> {
   const context = getContext();
+
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
   const result = await angularAppEngine.handle(request, context);
   return result || new Response('Not found', { status: 404 });
 }
